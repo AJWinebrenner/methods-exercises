9e2463bc68fea6d7a55772df1f686fe3eabe6649
Andrew Winebrenner <andrewjwinebrenner@gmail.com>
2022-01-27 12:05:17 +0000

    first commit

---
+++ src/Exercise_5.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Exercise_5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(capFirstLetter(scanner.nextLine()));
+    }
+
+    public static String capFirstLetter(String string) {
+        return string.substring(0,1).toUpperCase() + string.substring(1);
+    }
+}
